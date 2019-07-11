4db24a835b34cbc9648d347b7db18ea4e6379e18
Omar Bautista <obautista@skytouchtechnology.com>
2019-07-11 11:19:07 -0700

    Remove unused code

--- spring-boot-front/src/main/groovy/com/nearsoft/controller/IndexController.groovy
+++ spring-boot-front/src/main/groovy/com/nearsoft/controller/IndexController.groovy
@@ -1,13 +1,9 @@
 package com.nearsoft.controller
 
 import groovy.util.logging.Slf4j
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-
-import javax.annotation.PostConstruct
 
 @Controller
 @Slf4j
